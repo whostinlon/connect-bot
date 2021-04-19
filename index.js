ecbed7c343ef20d6d9c8a1e00b70b79bb67f3c63
Tin Lon Chiu <53127294+whostinlon@users.noreply.github.com>
2021-04-19 13:05:21 -0400

    finalized?

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ client.once('ready', () => {
 	client.user.setPresence({ activity: { name: 'for !connect', type: 'WATCHING' }, status: 'dnd' });
 });
 client.on('message', message => {
+	const errorMessage = new discordjs.MessageEmbed()
+		.setColor('#E21A23')
+		.setTitle('Error')
+		.setAuthor('Connect Bot', 'https://www.mheducation.com/content/dam/mhe/webassets/og/MHE_logo.png', 'https://newconnect.mheducation.com/')
+		.setTimestamp()
+		.setFooter(process.env.class + '\nRequested by: ' + message.author.tag);
 	if (message.content === '!connect soon' && message.author.username != client.username) {
 		const assignmentsDueSoon = [];
-		const errorMessage = new discordjs.MessageEmbed()
-			.setColor('#E21A23')
-			.setTitle('Error')
+		const loading = new discordjs.MessageEmbed()
+			.setColor('#1873E8')
+			.setTitle('Loading Prompt')
 			.setAuthor('Connect Bot', 'https://www.mheducation.com/content/dam/mhe/webassets/og/MHE_logo.png', 'https://newconnect.mheducation.com/')
-			.setDescription('A unexpected error occurred. Contact yum yum chicken yum yum#2288 for help.')
+			.setDescription('Starting up Connect instance...')
 			.setTimestamp()
 			.setFooter(process.env.class + '\nRequested by: ' + message.author.tag);
 		(async () => {
 			console.log('Processing request for: ' + message.author.tag);
+			const msg = await message.channel.send(loading);
 			const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 			const page = await browser.newPage();
 			await page.goto('https://newconnect.mheducation.com/');
@@ -27,25 +34,30 @@ client.on('message', message => {
 			try {
 				await page.type('#login-email', process.env.email);
 				await page.type('#login-password', process.env.password);
-				console.log('Sucessfully typed in credentials.');
+				console.log('Successfully typed in credentials.');
+				msg.edit(loading.setDescription('Authorization in progress...'));
 			}
 			catch (error) {
-				console.error('An unexpected login error occured (Username/Password): ' + error.message);
+				console.error('An unexpected login error occurred (Username/Password): ' + error.message);
+				msg.edit(errorMessage.setDescription('A unexpected error occurred during authorization. Contact yum yum chicken yum yum#2288 for help. (206)'));
 			}
 			try {
 				await page.click('#login-submit-btn');
 				console.log('Logging in....');
 				await page.waitForSelector('#iframewrapper > iframe');
-				console.log('Sucessfully logged in!');
+				console.log('Successfully logged in!');
+				msg.edit(loading.setDescription('Successfully logged in...'));
 			}
 			catch (error) {
-				console.error('An unexpected login error occcured (Connect Home Page): ' + error.message);
+				console.error('An unexpected login error occurred (Connect Home Page): ' + error.message);
+				msg.edit(errorMessage.setDescription('A unexpected error occurred accessing the Connect Homepage. Contact yum yum chicken yum yum#2288 for help. (606)'));
 			}
 			const elementHandle = await page.$('#iframewrapper > iframe');
 			const frame = await elementHandle.contentFrame();
 			await frame.waitForSelector('#layoutWrapper > div > div > div > div.container-fluid.connect-assignment-container.connect-result-wrapper.set-aria-hidden > div > main > div > ul:nth-child(2)');
 			const assignmentsList = await frame.$$eval('#layoutWrapper > div > div > div > div.container-fluid.connect-assignment-container.connect-result-wrapper.set-aria-hidden > div > main > div > ul:nth-child(4) > li >  div.col-xs-11.assignment-title-container > div > div > div:nth-child(1) > div.assignment-title.inner-column-left > h3', el => el.map(e => e.innerText));
 			const dueDateList = await frame.$$eval('#layoutWrapper > div > div > div > div.container-fluid.connect-assignment-container.connect-result-wrapper.set-aria-hidden > div > main > div > ul:nth-child(4) > li >  div.col-xs-11.assignment-title-container > div > div > div:nth-child(2) > p > span.standard-gray-color.default-text.due-date.font-bold', el => el.map(e => e.innerText));
+			msg.edit(loading.setDescription('Successfully collected assignments...'));
 			const assignments = assignmentsList.map(e => {
 				const newE = e.replace(newLineSelect, '');
 				return Array.of(newE);
@@ -64,6 +76,12 @@ client.on('message', message => {
 				}
 			});
 			browser.close();
+			try {
+				msg.delete();
+			}
+			catch (error) {
+				message.channel.send(errorMessage.setDescription('A unexpected error occurred during cleaning up my messages (Do I have the permissions to delete my own messages?). Contact yum yum chicken yum yum#2288 or the server owner for help.'));
+			}
 		})().then(() => {
 			if (assignmentsDueSoon.length == 0) {
 				const assignmentsDue = new discordjs.MessageEmbed()
@@ -77,13 +95,13 @@ client.on('message', message => {
 					message.channel.send(assignmentsDue);
 				}
 				catch (error) {
-					message.channel.send(errorMessage);
+					message.channel.send(errorMessage.setDescription('A unexpected error occurred during message output. Contact yum yum chicken yum yum#2288 for help.'));
 				}
 			}
 			else {
 
 				const assignmentsDue = new discordjs.MessageEmbed()
-					.setColor('#ffa500')
+					.setColor('#E21A23')
 					.setTitle('Assignments due in the next 7 days')
 					.setAuthor('Connect Bot', 'https://www.mheducation.com/content/dam/mhe/webassets/og/MHE_logo.png', 'https://newconnect.mheducation.com/')
 					.setDescription(assignmentsDueSoon.map(e => {
@@ -95,7 +113,7 @@ client.on('message', message => {
 					message.channel.send(assignmentsDue);
 				}
 				catch (error) {
-					message.channel.send(errorMessage);
+					message.channel.send(errorMessage.setDescription('A unexpected error occurred during message output. Contact yum yum chicken yum yum#2288 for help.'));
 				}
 			}
 		});
@@ -112,7 +130,12 @@ client.on('message', message => {
 				{ name: '!connect soon', value: 'Gathers the assignments due in the next 7 days.' },
 				{ name: '!connect/!connect help', value: 'Provides information about the bot' },
 			);
-		message.channel.send(helpMenu);
+		try {
+			message.channel.send(helpMenu);
+		}
+		catch (error) {
+			message.channel.send(errorMessage.setDescription('A unexpected error occurred during message output. Contact yum yum chicken yum yum#2288 for help.'));
+		}
 	}
 });
 
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Connect Bot
+
+This bot makes it easier for students to check upcoming assignments on the popular Connect platform from McGraw Hill Education. No copyright infringement was intended in the creation of the bot and all trademarks are owned by their respective owners. This bot should be used for educational purposes only.
+
+## Dependencies
+
+- Node.js
+- Discord.js
+- Puppeteer
+
+## Setup
+
+This bot was meant to be hosted on Heroku or another cloud hosting service. If you want to host this bot locally, replace the process.env variables with your token, email, password, and the class(es) you are currently taking.
+
+**The bot also requires the permissions to manage messages on whichever server it is put on.**
+
+## Future Development
+
+- Listing assignments due more than 7 days in advance. (Traversing through all classes to gather all assignments due in the future)
